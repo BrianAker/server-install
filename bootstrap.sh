ede0042cc234f9bd897b2e223f398479c6b82cec
Brian Aker <brian@tangent.org>
2012-09-04 21:04:55 -0700

    Fix for different distributions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ $(uname) = "ubuntu" ];
-then
+if [ -f "/etc/redhat-release"  ]; then
   sudo apt-get install -y git make
-else
+  make -f ubuntu.am
+elif [ -f "/etc/redhat-release"  ]; then
   sudo yum install -y git make
+  make -f fedora.am
+elif [ -d "/etc/mach_init.d"  ]; then
+  ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+  make -f osx.am
 fi
-make
-
