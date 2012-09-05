b6324d0f45ada66b34136e3475829d5a26fd7b3a
Brian Aker <brian@tangent.org>
2012-09-04 21:18:45 -0700

    Actually works :)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 if [ -f "/etc/redhat-release"  ]; then
-  sudo apt-get install -y git make
-elif [ -f "/etc/redhat-release"  ]; then
   sudo yum install -y git make
+elif [ -f "/etc/debconf.conf"  ]; then
+  sudo apt-get update
+  sudo apt-get install -y git make
 elif [ -d "/etc/mach_init.d"  ]; then
-  ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+  echo "Install homebrew"
+#  ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 fi
 
 git clone https://github.com/BrianAker/server-install.git
 
-if [ -f "/etc/redhat-release"  ]; then
+if [ -f "/etc/debconf.conf"  ]; then
   make -f server-install/ubuntu.am
 elif [ -f "/etc/redhat-release"  ]; then
   make -f server-install/fedora.am
