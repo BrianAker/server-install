d55f322bd55869c558c78f0041a71e965c0ddbbd
Brian Aker <brian@tangent.org>
2012-11-01 18:35:32 -0700

    Update for reboot and use git-core for Ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-if [ -f "/etc/redhat-release"  ]; then
-  sudo yum install -y git make
-elif [ -f "/etc/debconf.conf"  ]; then
-  sudo apt-get update
-  sudo apt-get install -y git make
-elif [ -d "/etc/mach_init.d"  ]; then
-  if [ ! -f /usr/local/bin/brew ]; then
-    echo "Install homebrew"
-    `ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)`
+function init()
+{
+  if [ -f "/etc/redhat-release"  ]; then
+    sudo yum install -y git make
+  elif [ -f "/etc/debconf.conf"  ]; then
+    sudo apt-get update -y
+    sudo apt-get install -y git-core make
+  elif [ -d "/etc/mach_init.d"  ]; then
+    if [ ! -f /usr/local/bin/brew ]; then
+      echo "Install homebrew"
+      `ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)`
+    fi
   fi
-fi
+}
 
 # If the fedora.am file is around we know then that we are in the repository
-if [ -f 'yum.am' ]; then
-  git pull
-  make
-elif [ -d 'server-install' ]; then
-  (cd server-install && git pull && make)
-else
-  git clone https://github.com/BrianAker/server-install.git
-  (cd server-install && make)
-fi
+function run_server_install()
+{
+  if [ -f 'yum.am' ]; then
+    git pull
+    make
+  elif [ -d 'server-install' ]; then
+    (cd server-install && git pull && make)
+  else
+    git clone https://github.com/BrianAker/server-install.git
+    (cd server-install && make)
+  fi
+}
+
+init
+run_server_install
+
