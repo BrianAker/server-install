cb980e45604b878ad17e0b7136d31041a50db3e9
Brian Aker <brian@tangent.org>
2013-03-10 04:55:48 -0700

    Add propper quoting.

--- generate-keystonerc.sh
+++ generate-keystonerc.sh
@@ -7,7 +7,6 @@ function keystonerc()
     return 1
   else
     echo $KEYSTONERC
-    touch $KEYSTONERC
 cat > $KEYSTONERC << _EOF
 export ADMIN_TOKEN=$(openssl rand -hex 10)
 export OS_USERNAME=admin
@@ -15,7 +14,7 @@ export OS_PASSWORD=verybadpass
 export OS_TENANT_NAME=admin
 export OS_AUTH_URL=http://127.0.0.1:5000/v2.0/
 export SERVICE_ENDPOINT=http://127.0.0.1:35357/v2.0/
-export SERVICE_TOKEN="$ADMIN_TOKEN"
+export SERVICE_TOKEN=\$ADMIN_TOKEN
 _EOF
 fi
 }
