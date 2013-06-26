1879c3f93f792a7458ddbf9113a03b95504d9195
Brian Aker <brian@tangent.org>
2013-06-25 21:19:19 -0700

    Fix account add.

--- dist.mk
+++ dist.mk
@@ -9,35 +9,42 @@ FREEBSD_RELEASE := $(shell test -x '/usr/sbin/pkg_add' > /dev/null 2>&1 ; echo $
 OSX_RELEASE := $(shell test -d '/etc/mach_init.d' > /dev/null 2>&1 ; echo $$?)
 
 ifneq (${DEB_CONF},)
+  DEB_CONF:= 1
   DISTRIBUTION := ubuntu
   include $(srcdir)/apt.am
   include $(srcdir)/ubuntu.am
   include $(srcdir)/accounts.am
 else ifneq (${SUSE_RELEASE},)
+  SUSE_RELEASE:= 1
   DISTRIBUTION := suse
   include zypper.am
   include $(srcdir)/accounts.am
 else ifneq (${FEDORA_RELEASE},)
+  FEDORA_RELEASE:= 1
   DISTRIBUTION := fedora
   include $(srcdir)/yum.mk
   include $(srcdir)/fedora.am
   include $(srcdir)/accounts.am
 else ifneq (${CENTOS_RELEASE},)
+  CENTOS_RELEASE:= 1
   DISTRIBUTION := centos
   include $(srcdir)/yum.am
   include $(srcdir)/centos.mk
   include $(srcdir)/accounts.am
 else ifneq (${RHEL_RELEASE},)
+  RHEL_RELEASE:= 1
   DISTRIBUTION := rhel
   include $(srcdir)/yum.am
   include $(srcdir)/centos.mk
   include $(srcdir)/accounts.am
 else ifeq (${FREEBSD_RELEASE},0)
+  FREEBSD_RELEASE:= 1
   DISTRIBUTION := freebsd9
-  include freebsd.am
+  include $(srcdir)/freebsd.am
 else ifeq (${OSX_RELEASE},0)
+  OSX_RELEASE:= 1
   DISTRIBUTION := osx
-  include osx.am
+  include $(srcdir)/osx.am
 else
   DISTRIBUTION := unknown
 endif
