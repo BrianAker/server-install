2b8e44a33f8e621250d63078f674a732ac545a17
Brian Aker <brian@tangent.org>
2014-03-02 16:16:19 -0800

    Remove dead files.

--- yum.mk
+++
@@ -1,8 +0,0 @@
-# vim:ft=automake
-#
-YUM:= yum -y --quiet
-PKG_CACHE:= $(YUM) makecache
-PKG_INSTALLER:= $(YUM) install
-PKG_PROVIDES:= $(YUM) whatprovides
-PKG_UPGRADE:= $(YUM) upgrade
-PKG_SEARCH_INSTALL= $(PKG_INSTALLER) $(1) || ($(PKG_PROVIDES) $(1) | head -1 | cut -d : -f 1 | xargs $(PKG_INSTALLER))
